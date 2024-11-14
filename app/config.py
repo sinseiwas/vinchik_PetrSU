60f941c96e7055bde463050f59ffa0d528192cac
Alexeeeeeeey <alexey.shaganov1@gmail.com>
2024-11-14 14:12:27 +0300

    send photo hotfix

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-PHOTO_FOLDER = os.getenv("PHOTO_FOLDER")
+PHOTO_FOLDER = os.path.abspath(os.getenv("PHOTO_FOLDER"))

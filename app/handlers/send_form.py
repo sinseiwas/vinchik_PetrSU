c404db4e0ac743b4691d7b569eb9c6598a48c205
danil <d-suhanuv@mail.ru>
2025-02-08 21:21:56 +0300

    edits

--- app/handlers/send_form.py
+++ app/handlers/send_form.py
@@ -7,14 +7,15 @@ from aiogram.filters import Command
 from aiogram.types import Message, FSInputFile
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from database.users.crud import get_form_by_user
+from database.users.crud import get_form_by_user, get_user_from_id
 
 router = Router()
 
 
 @router.message(Command('my_form'))
 async def send_my_form(message: Message, session: AsyncSession):
-    form = await get_form_by_user(session, message.from_user.id)
+    user_id = await get_user_from_id(session, message.from_user.id)
+    form = await get_form_by_user(session, user_id)
 
     if form:
         response = lang.FORM_MESSAGE.format(
